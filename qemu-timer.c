16d1dc652db65c11af6854b4e90a288db557ee32
Lee Essen <lee.essen@nowonline.co.uk>
2012-03-16 18:01:40 +0000

    Add HIGHRES clock for sun

--- qemu-timer.c
+++ qemu-timer.c
@@ -533,7 +533,11 @@ static int dynticks_start_timer(struct qemu_alarm_timer *t)
 #endif /* SIGEV_THREAD_ID */
     ev.sigev_signo = SIGALRM;
 
+#if defined(__sun__)
+    if (timer_create(CLOCK_HIGHRES, &ev, &host_timer)) {
+#else
     if (timer_create(CLOCK_REALTIME, &ev, &host_timer)) {
+#endif
         perror("timer_create");
 
         /* disable dynticks */
